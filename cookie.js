5671e8664349f5962ba366c205b43de415eb1278
ecomimpactnl-crypto <ecomimpactnl@gmail.com>
2026-08-21 15:06:31 +0200

    Add files via upload

--- cookie.js
+++ cookie.js
@@ -14,7 +14,8 @@
   };
 
   window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
+  // globaal maken: index.html en app.js roepen gtag() ook aan
+  window.gtag = function gtag() { dataLayer.push(arguments); };
   gtag('consent', 'default', {
     ad_storage: 'denied',
     analytics_storage: 'denied',
